a040d04c6c670dcc0d696ff830c2381fd707d411
stealwater <water2924@gmail.com>
2018-09-06 17:26:09 +0800

    fixed auru error.

--- CompactRaid/Modules/CornerIndicators/Visual.lua
+++ CompactRaid/Modules/CornerIndicators/Visual.lua
@@ -31,10 +31,10 @@ local function FindAura(unit, aura, selfcast, lacks, similar)
 	if similar then
 		name, icon, count, dispelType, duration, expires, caster, harmful = auraGroups:UnitAura(unit, aura)
 	else
-		name, icon, count, _, duration, expires = UnitBuff(unit, aura, nil, filter)
+		name, icon, count, _, duration, expires = UnitBuff(unit, aura, filter)
 		if not name then
 			harmful = 1
-			name, icon, count, _, duration, expires = UnitDebuff(unit, aura, nil, filter)
+			name, icon, count, _, duration, expires = UnitDebuff(unit, aura, filter)
 		end
 	end
 
